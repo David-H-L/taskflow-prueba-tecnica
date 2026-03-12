2476a941ba32422382ca22ffeaef32d9998d2383
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2026-03-12 13:16:30 -0400

    feat add project card

---
+++ src/components/projects/projectCard.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import { ProjectCardProps } from '@/types/index';
+
+export default function ProjectCard({
+  id,
+  name,
+  description,
+  color,
+  tasks,
+}: ProjectCardProps) {
+  return (
+    <Link href={`/projects/${id}`}>
+      <article className="bg-white rounded-xl shadow-md cursor-pointer p-5 flex flex-col gap-4 ">
+        <header className="flex flex-col gap-3">
+          <div className="flex justify-between">
+            <h3 className="font-semibold text-lg line-clamp-1">{name}</h3>
+            <span
+              className="w-5 h-5 rounded-full"
+              style={{ backgroundColor: color }}
+            />
+          </div>
+          <div>
+            <p className="text-gray-500 text-sm line-clamp-2">{description}</p>
+          </div>
+        </header>
+
+        <footer className="grid grid-cols-3 gap-4 mt-auto text-sm">
+          <div className="flex flex-col">
+            <span className="text-gray-500">Completed</span>
+            <span className="font-semibold text-green-600">
+              {tasks.completed}
+            </span>
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-gray-500">In Progress</span>
+            <span className="font-semibold text-blue-600">
+              {tasks.inProgress}
+            </span>
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-gray-500">Pending</span>
+            <span className="font-semibold text-orange-500">
+              {tasks.pending}
+            </span>
+          </div>
+        </footer>
+      </article>
+    </Link>
+  );
+}
